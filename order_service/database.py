3780fa91360c91d53f92d1e1f871000b60aa2afc
Maxim Vassilev <maximvasilev01@gmail.com>
2025-11-10 17:56:56 +0200

    order_service - fixed

--- order_service/database.py
+++ order_service/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5433/orders_db"
+DATABASE_URL = "postgresql+psycopg2://user_1:password@localhost:5433/orders_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
